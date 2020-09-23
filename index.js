7be6997b72ec106c4737783ce5c5b28e4b2b204e
hcjesus <53020713+hcjesus@users.noreply.github.com>
2020-09-23 00:35:52 -0500

    triinga

--- index.js
+++ index.js
@@ -3,22 +3,21 @@ require("./db");
 const express = require("express");
 const app = express();
 const cors = require("cors");
-const PORT = process.env.PORT || 4000;
+const port = process.env.PORT || 4000;
 const productos = require("./controllers/productosController");
 const mascotas = require("./controllers/mascotasController");
 const citas = require("./controllers/citasController");
 const vacunas = require("./controllers/vacunasController");
 const bodyParser = require("body-parser");
 
-app.set('PORT',PORT);
 
 //app.use(cors({origin:'http://localhost:3000'}));
 app.use(cors());
 
 app.use(bodyParser.json());
 
-app.listen(PORT,()=>{
-	console.log("PORT: ", PORT);
+app.listen(port,()=>{
+	console.log("PORT: ", port);
 });
 
 app.use('/productos',productos);
